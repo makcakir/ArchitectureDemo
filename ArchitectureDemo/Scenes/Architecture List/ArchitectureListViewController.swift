36849c31e909ba2ac0561ec4a8fd2126277026e4
Mustafa Ali Akçakır <mustafaaliakcakir@gmail.com>
2018-10-11 11:27:45 +0300

    Add ArchitectureListViewController

---
+++ ArchitectureDemo/Scenes/Architecture List/ArchitectureListViewController.swift
@@ -0,0 +1,72 @@
+//
+//  ArchitectureListViewController.swift
+//  ArchitectureDemo
+//
+//  Created by Mustafa Ali Akçakır on 10/10/2018.
+//  Copyright © 2018 Mustafa Ali Akçakır. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+typealias ArchitecturePresentation = (title: String, detail: String)
+
+class ArchitectureListViewController: UITableViewController {
+
+    private enum Const {
+        static let cellIdentifier = "ArchitectureCell"
+    }
+
+    var architectures: [ArchitecturePresentation] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        initializeViews()
+    }
+}
+
+// MARK: - Setup
+
+private extension ArchitectureListViewController {
+
+    func initializeViews() {
+        title = "Architecture Demo"
+        tableView.tableFooterView = UIView()
+    }
+}
+
+// MARK: - UITableViewDataSource
+
+extension ArchitectureListViewController {
+
+    override func tableView(_ tableView: UITableView,
+                            numberOfRowsInSection section: Int) -> Int {
+        return architectures.count
+    }
+
+    override func tableView(_ tableView: UITableView,
+                            cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: Const.cellIdentifier)
+        if cell == nil {
+            cell = UITableViewCell(style: .subtitle, reuseIdentifier: Const.cellIdentifier)
+            cell?.accessoryType = .disclosureIndicator
+        }
+
+        let architecture = architectures[indexPath.row]
+
+        cell?.textLabel?.text = architecture.title
+        cell?.detailTextLabel?.text = architecture.detail
+
+        return cell ?? UITableViewCell()
+    }
+}
+
+// MARK: - UITableViewDelegate
+
+extension ArchitectureListViewController {
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        #warning ("Will be implemented!")
+    }
+}
