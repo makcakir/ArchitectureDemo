a660183ef1dc82d62dd5b61419a50540c753c99c
Mustafa Ali Akçakır <mustafaaliakcakir@gmail.com>
2018-10-24 15:51:27 +0300

    Implement MVCFibonacciViewController

--- ArchitectureDemo/Scenes/Fibonacci/MVC/MVCFibonacciViewController.swift
+++ ArchitectureDemo/Scenes/Fibonacci/MVC/MVCFibonacciViewController.swift
@@ -6,11 +6,55 @@
 //  Copyright © 2018 Mustafa Ali Akçakır. All rights reserved.
 //
 
+import Foundation
+import UIKit
+
 class MVCFibonacciViewController: FibonacciViewController {
+
+    private enum Const {
+        static let counterKey = "com.makcakir.ArchitectureDemo.counter.mvc"
+    }
+
+    private var counter: Int = 0
+    private let formatter = NumberFormatter()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        #warning ("will be implemented!")
+        counter = UserDefaults.standard.integer(forKey: Const.counterKey)
+        formatter.numberStyle = .spellOut
+        updateViews()
+    }
+
+    override func decreaseButtonTapped(_ sender: UIButton) {
+        super.decreaseButtonTapped(sender)
+
+        counter -= 1
+        handleCounterChange()
+    }
+
+    override func increaseButtonTapped(_ sender: UIButton) {
+        super.increaseButtonTapped(sender)
+
+        counter += 1
+        handleCounterChange()
+    }
+}
+
+private extension MVCFibonacciViewController {
+
+    func updateViews() {
+        #warning ("change image view visibility according to fibonacci rule!")
+
+        let numberValue = NSNumber(value: counter)
+        integerLabel.text = formatter.string(from: numberValue)?.uppercased()
+        integerLabel.textColor = counter % 2 == 0 ? UIColor.yellow : UIColor.red
+
+        decreaseButton.isEnabled = counter > 0
+    }
+
+    func handleCounterChange() {
+        UserDefaults.standard.set(counter, forKey: Const.counterKey)
+        updateViews()
     }
 }
