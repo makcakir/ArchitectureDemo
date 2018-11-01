eb48a92c12122708b20d1695755e5d3df5e5061b
Mustafa Ali Akçakır <mustafaaliakcakir@gmail.com>
2018-11-01 10:57:44 +0300

    Add VIPERFibonacci

---
+++ ArchitectureDemo/Scenes/Fibonacci/VIPER/VIPERFibonacci.swift
@@ -0,0 +1,30 @@
+//
+//  VIPERFibonacci.swift
+//  ArchitectureDemo
+//
+//  Created by Mustafa Ali Akçakır on 01/11/2018.
+//  Copyright © 2018 Mustafa Ali Akçakır. All rights reserved.
+//
+
+import Foundation
+
+protocol VIPERFibonacciInteractorInputProtocol {
+    func retrieveInitialCounter()
+    func decreaseCounter()
+    func increaseCounter()
+}
+
+protocol VIPERFibonacciInteractorOutputProtocol: class {
+    func counterUpdated(_ counter: Int)
+}
+
+protocol VIPERFibonacciPresentingProtocol {
+    func prepareInitialPresentation()
+    func decrease()
+    func increase()
+}
+
+protocol VIPERFibonacciViewProtocol: class {
+    func fibonacciPresentationUpdated(_ presentation: FibonacciPresentation)
+    func alertMessageChanged(_ alertMessage: String)
+}
